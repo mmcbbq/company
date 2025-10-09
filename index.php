9ae820c6c8cc61fe10adf54cb97dc924e2a64893
mmcbbq <m.martinez@bbq.de>
2025-10-09 08:58:26 +0200

    index

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+?>
+
+
+<!doctype html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport'
+          content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <div>Department</div>
+    <div>
+        <ul>
+            <li><a href='create_department.php'>create</a></li>
+            <li><a href='read_department.php'>show</a></li>
+
+        </ul>
+    </div>
+</div>
+<div>Employees</div>
+<div>
+    <ul>
+        <li><a href='firstcreate.php'>create</a></li>
+        <li><a href='firstread.php'>show</a></li>
+
+    </ul>
+</div>
+</div>
+
+</body>
+</html>
